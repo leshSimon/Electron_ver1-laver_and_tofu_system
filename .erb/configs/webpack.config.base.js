142dc6f83aa476295355d82293e02c6adfe00038
Forceps <54647455+Forceps@users.noreply.github.com>
2021-02-10 17:39:37 +0900

    s
    s

--- .erb/configs/webpack.config.base.js
+++ .erb/configs/webpack.config.base.js
@@ -35,7 +35,7 @@ export default {
    * Determine the array of extensions that should be used to resolve modules.
    */
   resolve: {
-    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx', '.exe'],
     modules: [path.join(__dirname, '../src'), 'node_modules'],
   },
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,23 +3,13 @@ import { render } from 'react-dom';
 import ContextApply from './src_editable/Routes/AppRoot/ContextApply';
 import Helmet from 'react-helmet';
 import './src_editable/GlobalLib/Styles/GlobalStyle/ImportGlobalCss.global.scss';
-import { createConnection } from 'typeorm';
 
-createConnection({
-  type: 'sqlite',
-  synchronize: true,
-  logging: true,
-  logger: 'simple-console',
-  database: './Assets/ExternalFile/LavertuDatabase.db',
-  entities: [],
-}).then(() => {
-  render(
-    <>
-      <Helmet>
-        <title>Lavertu</title>
-      </Helmet>
-      <ContextApply />
-    </>,
-    document.getElementById('root')
-  );
-});
+render(
+  <>
+    <Helmet>
+      <title>Lavertu</title>
+    </Helmet>
+    <ContextApply />
+  </>,
+  document.getElementById('root')
+);
